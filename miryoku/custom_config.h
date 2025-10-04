0b23e66367d270e1e440c65b0d17280ee4edb225
Tim Waterhouse <t.waterhouse@gmail.com>
2025-10-04 10:06:22 -0400

    add gallium layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,15 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_BASE \
+&kp B,             &kp L,             &kp D,             &kp C,             &kp V,             &kp J,             &kp Y,             &kp O,             &kp U,             &kp SQT,             \
+U_MT(LGUI, N),     U_MT(LALT, R),     U_MT(LCTRL, T),    U_MT(LSHFT, S),    &kp G,             &kp P,             U_MT(LSHFT, H),    U_MT(LCTRL, A),    U_MT(LALT, E),     U_MT(LGUI, I),   \
+U_LT(U_BUTTON, X), U_MT(RALT, Q),     &kp M,             &kp W,             &kp Z,             &kp K,             &kp F,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_TAP \
+&kp B,             &kp L,             &kp D,             &kp C,             &kp V,             &kp J,             &kp Y,             &kp O,             &kp U,             &kp SQT,             \
+&kp N,             &kp R,             &kp T,             &kp S,             &kp G,             &kp P,             &kp H,             &kp A,             &kp E,             &kp I,           \
+&kp X,             &kp Q,             &kp M,             &kp W,             &kp Z,             &kp K,             &kp F,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+
